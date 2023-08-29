b1d8d0aa7afff443c852acd87d328ba7c2d35021
jayant pal <jpofficials7@gmail.com>
2023-08-29 18:23:47 +0530

    final setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
+console.log(data);
+
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <>
       <div>
         <Navbar />
-        {/* <Hero /> */}
-        <Card />
+        <Hero />
+        {cards}
       </div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import KatieImage from "../images/katie-zaferes.png";
 import StarImage from "../images/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <>
       <div className="card">
-        <img src={KatieImage} alt="katie" className="card--image" />
+        <img
+          src={require(`../images/katie-zaferes.png`)}
+          alt="katie"
+          className="card--image"
+        />
         <div className="card--stats">
           <img src={StarImage} alt="start" className="card-star" />
-          <span>5.0</span>
-          <span className="gray">(6).</span>
-          <span className="gray">USA</span>
+          <span>{props.rating}</span>
+          <span className="gray">({props.reviewCount}).</span>
+          <span className="gray">{props.location}</span>
         </div>
-        <p>Life Lessons with katie zaferas</p>
+        <p>{props.title}</p>
         <p>
-          <span className="bold">From $136</span> / person
+          <span className="bold">From ${props.price}</span> / person
         </p>
       </div>
     </>
